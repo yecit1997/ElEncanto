1cffcba7ad2fe8eac335be8a4ab2884ee1b1cb1c
yecit1997 <123273193+yecit1997@users.noreply.github.com>
2023-01-21 16:45:31 -0500

    Add files via upload

---
+++ estilosjava.js
@@ -0,0 +1,37 @@
+
+var confianza, equipo, dialogo, convivencia;
+
+function comenzar(){
+    confianza=document.getElementById("confianza");
+    confianza.addEventListener("click",Mconfianza,false);
+
+    equipo=document.getElementById("equipo");
+    equipo.addEventListener("click",Mequipo,false);
+
+    dialogo=document.getElementById("dialogo");
+    dialogo.addEventListener("click",Mdialogo,false);
+
+    convivencia=document.getElementById("convivencia");
+    convivencia.addEventListener("click",Mconvivencia,false);
+
+    
+}
+
+function Mconfianza(){
+    window.open("http://127.0.0.1:5500/convivencia.html")
+    
+}
+
+function Mequipo(){
+    window.open("http://127.0.0.1:5500/equipo.html");
+}
+
+function Mdialogo(){
+    window.open("http://127.0.0.1:5500/dialogo.html")
+}
+
+function Mconvivencia(){
+    window.open("http://127.0.0.1:5500/convivencia.html")
+}
+
+window.onload=comenzar;
